f70cc120e44e23e04448a63703f45bc682335e9a
omrltff <olatif04@gmail.com>
2024-02-08 14:47:54 -0500

    home page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,22 @@
 import { useHistory } from 'react-router-dom';
-import ShowListings from './ShowListings';
 function Home() {
     
-    const history = useHistory();//for routing easily
+    //const history = useHistory();//for routing easily
     
+    const handleSpotifyLogin = (e) => {
+        e.preventDefault();
+        const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+        const redirect_uri = encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
+        const scopes = encodeURIComponent('user-read-private user-read-email'); // Scopes separated by spaces
+        const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=code&show_dialog=true`;
+        window.location.href = spotifyAuthUrl;
+    };
+
   return (
     <div className="Home">
       <header className="header">
         <h1>Elementify</h1>
-        <button className='button' onClick={goToSellPage}>Log In With Spotify</button>
+        <button className='button' onClick={handleSpotifyLogin}>Log In With Spotify</button>
       </header>
 
     </div>
