324ab070b28f8c1c26da5e75047cd08940b00ca4
omrltff <olatif04@gmail.com>
2024-02-08 22:28:04 -0500

    error logs

--- src/Home.js
+++ src/Home.js
@@ -29,8 +29,19 @@ function Home() {
         console.log(data); // Your top tracks will be logged in the console.
         // Process and use data in your app
       } catch (error) {
-        console.error('Error fetching top tracks:', error);
-        // Handle errors, e.g., token expiration
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error("Error data:", error.response.data);
+          console.error("Error status:", error.response.status);
+          console.error("Error headers:", error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error("Error request:", error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error("Error message:", error.message);
+        }
+        console.error("Error config:", error.config);
       }
     };
     
