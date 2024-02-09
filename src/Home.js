a67d5fbcbf4ba03bcda9e24c5d33dfefbbe5bd89
omrltff <olatif04@gmail.com>
2024-02-08 21:53:49 -0500

    login button

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,8 @@ import { useHistory } from 'react-router-dom';
 function Home() {
     
     //const history = useHistory();//for routing easily
-    
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
     const handleSpotifyLogin = (e) => {
         e.preventDefault();
         const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
@@ -11,12 +12,22 @@ function Home() {
         const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=code&show_dialog=true`;
         window.location.href = spotifyAuthUrl;
     };
+    const handleLogout = () => {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('isLoggedIn');
+      //redirect to home page
+      window.location.href = '/';
+    };
+    
 
   return (
     <div className="Home">
       <header className="header">
         <h1>Elementify</h1>
         <button className='button' onClick={handleSpotifyLogin}>Log In With Spotify</button>
+        {isLoggedIn && <p className="logged-in-text">Logged In</p>}
+        {isLoggedIn && <button onClick={handleLogout}>Log Out</button>}
+
       </header>
 
     </div>
