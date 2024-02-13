50f707670d134332bc18a2e3d4400514cb8d4529
omrltff <olatif04@gmail.com>
2024-02-13 11:26:50 -0500

    spotify scopes

--- src/Home.js
+++ src/Home.js
@@ -71,7 +71,7 @@ function Home() {
     e.preventDefault();
     const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
     const redirect_uri = encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
-    const scopes = encodeURIComponent('user-read-private user-read-email user-top-read');
+    const scopes = encodeURIComponent('user-read-private user-read-email user-top-read playlist-read-private playlist-read-collaborative');
     const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=code&show_dialog=true`;
     window.location.href = spotifyAuthUrl;
   };
